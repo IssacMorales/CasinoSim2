6cfb609025a30697a9db4bc7c0878c67c265c6ff
IssacMorales <imorales@student.neumont.edu>
2023-02-09 10:29:15 -0700

    Title

--- CasinoSim/CasinoSim/Form1.Designer.cs
+++ CasinoSim/CasinoSim/Form1.Designer.cs
@@ -33,11 +33,15 @@
             this.bJackbtn = new System.Windows.Forms.Button();
             this.Roulettebtn = new System.Windows.Forms.Button();
             this.Slotbtn = new System.Windows.Forms.Button();
+            this.Money = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pokerbtn = new System.Windows.Forms.Button();
+            this.Creditbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Crapsbtn
             // 
-            this.Crapsbtn.Location = new System.Drawing.Point(127, 377);
+            this.Crapsbtn.Location = new System.Drawing.Point(125, 324);
             this.Crapsbtn.Name = "Crapsbtn";
             this.Crapsbtn.Size = new System.Drawing.Size(264, 76);
             this.Crapsbtn.TabIndex = 0;
@@ -47,45 +51,88 @@
             // Title
             // 
             this.Title.AutoSize = true;
-            this.Title.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Title.Location = new System.Drawing.Point(407, 54);
+            this.Title.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Title.Location = new System.Drawing.Point(310, 44);
             this.Title.Name = "Title";
-            this.Title.Size = new System.Drawing.Size(416, 54);
+            this.Title.Size = new System.Drawing.Size(626, 81);
             this.Title.TabIndex = 1;
             this.Title.Text = "Casino-Man Simulator";
             // 
             // bJackbtn
             // 
-            this.bJackbtn.Location = new System.Drawing.Point(864, 377);
+            this.bJackbtn.Location = new System.Drawing.Point(862, 324);
             this.bJackbtn.Name = "bJackbtn";
             this.bJackbtn.Size = new System.Drawing.Size(264, 76);
             this.bJackbtn.TabIndex = 2;
-            this.bJackbtn.Text = "black Jack (coming soon)";
+            this.bJackbtn.Text = "Black Jack (coming soon)";
             this.bJackbtn.UseVisualStyleBackColor = true;
             // 
             // Roulettebtn
             // 
-            this.Roulettebtn.Location = new System.Drawing.Point(127, 511);
+            this.Roulettebtn.Location = new System.Drawing.Point(125, 458);
             this.Roulettebtn.Name = "Roulettebtn";
             this.Roulettebtn.Size = new System.Drawing.Size(264, 76);
             this.Roulettebtn.TabIndex = 3;
             this.Roulettebtn.Text = "Roulette (coming soon)";
             this.Roulettebtn.UseVisualStyleBackColor = true;
+            this.Roulettebtn.Click += new System.EventHandler(this.Roulettebtn_Click);
             // 
             // Slotbtn
             // 
-            this.Slotbtn.Location = new System.Drawing.Point(864, 511);
+            this.Slotbtn.Location = new System.Drawing.Point(862, 458);
             this.Slotbtn.Name = "Slotbtn";
             this.Slotbtn.Size = new System.Drawing.Size(264, 76);
             this.Slotbtn.TabIndex = 4;
             this.Slotbtn.Text = "Slots (coming soon)";
             this.Slotbtn.UseVisualStyleBackColor = true;
             // 
+            // Money
+            // 
+            this.Money.AutoSize = true;
+            this.Money.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Money.Location = new System.Drawing.Point(713, 196);
+            this.Money.Name = "Money";
+            this.Money.Size = new System.Drawing.Size(82, 41);
+            this.Money.TabIndex = 8;
+            this.Money.Text = "1000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(453, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 41);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Money Available: $";
+            // 
+            // Pokerbtn
+            // 
+            this.Pokerbtn.Location = new System.Drawing.Point(494, 397);
+            this.Pokerbtn.Name = "Pokerbtn";
+            this.Pokerbtn.Size = new System.Drawing.Size(264, 76);
+            this.Pokerbtn.TabIndex = 9;
+            this.Pokerbtn.Text = "Poker (coming soon)";
+            this.Pokerbtn.UseVisualStyleBackColor = true;
+            // 
+            // Creditbtn
+            // 
+            this.Creditbtn.Location = new System.Drawing.Point(1115, 606);
+            this.Creditbtn.Name = "Creditbtn";
+            this.Creditbtn.Size = new System.Drawing.Size(112, 34);
+            this.Creditbtn.TabIndex = 10;
+            this.Creditbtn.Text = "Credits";
+            this.Creditbtn.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1239, 652);
+            this.Controls.Add(this.Creditbtn);
+            this.Controls.Add(this.Pokerbtn);
+            this.Controls.Add(this.Money);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Slotbtn);
             this.Controls.Add(this.Roulettebtn);
             this.Controls.Add(this.bJackbtn);
@@ -105,5 +152,9 @@
         private Button bJackbtn;
         private Button Roulettebtn;
         private Button Slotbtn;
+        private Label Money;
+        private Label label1;
+        private Button Pokerbtn;
+        private Button Creditbtn;
     }
 }
---
+++ CasinoSim/CasinoSim/Roulette.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CasinoSim
+{
+    partial class Roulette
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Roulette));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(450, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This do seem like Roulette";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(57, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1034, 341);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Roulette
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 619);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Roulette";
+            this.Text = "Roulette";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+    }
+}
