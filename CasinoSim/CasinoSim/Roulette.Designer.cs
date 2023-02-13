a8eab65caf22e45c401ade7a81bac49f9d6df38d
IssacMorales <imorales@student.neumont.edu>
2023-02-13 16:32:25 -0700

    ban

--- CasinoSim/CasinoSim/Roulette.Designer.cs
+++ CasinoSim/CasinoSim/Roulette.Designer.cs
@@ -29,35 +29,26 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Roulette));
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.black = new System.Windows.Forms.Button();
             this.Red = new System.Windows.Forms.Button();
             this.twelve = new System.Windows.Forms.Button();
             this.twenty = new System.Windows.Forms.Button();
             this.thirty = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.num = new System.Windows.Forms.TextBox();
             this.custom = new System.Windows.Forms.Button();
             this.CB = new System.Windows.Forms.Label();
             this.High = new System.Windows.Forms.Button();
             this.low = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BetAmount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(450, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(218, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "This do seem like Roulette";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(57, 98);
+            this.pictureBox1.Location = new System.Drawing.Point(71, 56);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1034, 341);
             this.pictureBox1.TabIndex = 1;
@@ -65,7 +56,7 @@
             // 
             // black
             // 
-            this.black.Location = new System.Drawing.Point(556, 454);
+            this.black.Location = new System.Drawing.Point(689, 435);
             this.black.Name = "black";
             this.black.Size = new System.Drawing.Size(112, 34);
             this.black.TabIndex = 2;
@@ -74,7 +65,7 @@
             // 
             // Red
             // 
-            this.Red.Location = new System.Drawing.Point(711, 454);
+            this.Red.Location = new System.Drawing.Point(844, 435);
             this.Red.Name = "Red";
             this.Red.Size = new System.Drawing.Size(112, 34);
             this.Red.TabIndex = 3;
@@ -83,7 +74,7 @@
             // 
             // twelve
             // 
-            this.twelve.Location = new System.Drawing.Point(556, 510);
+            this.twelve.Location = new System.Drawing.Point(992, 435);
             this.twelve.Name = "twelve";
             this.twelve.Size = new System.Drawing.Size(112, 34);
             this.twelve.TabIndex = 4;
@@ -92,7 +83,7 @@
             // 
             // twenty
             // 
-            this.twenty.Location = new System.Drawing.Point(711, 510);
+            this.twenty.Location = new System.Drawing.Point(689, 492);
             this.twenty.Name = "twenty";
             this.twenty.Size = new System.Drawing.Size(112, 34);
             this.twenty.TabIndex = 5;
@@ -101,43 +92,45 @@
             // 
             // thirty
             // 
-            this.thirty.Location = new System.Drawing.Point(860, 454);
+            this.thirty.Location = new System.Drawing.Point(844, 492);
             this.thirty.Name = "thirty";
             this.thirty.Size = new System.Drawing.Size(112, 34);
             this.thirty.TabIndex = 6;
             this.thirty.Text = "25 - 36";
             this.thirty.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // num
             // 
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(532, 569);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 47);
-            this.textBox1.TabIndex = 8;
+            this.num.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num.Location = new System.Drawing.Point(844, 551);
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(112, 47);
+            this.num.TabIndex = 8;
+            this.num.Text = "number";
             // 
             // custom
             // 
-            this.custom.Location = new System.Drawing.Point(710, 567);
+            this.custom.Location = new System.Drawing.Point(992, 561);
             this.custom.Name = "custom";
             this.custom.Size = new System.Drawing.Size(112, 34);
             this.custom.TabIndex = 9;
             this.custom.Text = "Single";
             this.custom.UseVisualStyleBackColor = true;
+            this.custom.Click += new System.EventHandler(this.custom_Click);
             // 
             // CB
             // 
             this.CB.AutoSize = true;
             this.CB.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.CB.Location = new System.Drawing.Point(57, 522);
+            this.CB.Location = new System.Drawing.Point(36, 484);
             this.CB.Name = "CB";
-            this.CB.Size = new System.Drawing.Size(76, 41);
+            this.CB.Size = new System.Drawing.Size(176, 41);
             this.CB.TabIndex = 10;
-            this.CB.Text = "Bet: ";
+            this.CB.Text = "Bet Square: ";
             // 
             // High
             // 
-            this.High.Location = new System.Drawing.Point(860, 510);
+            this.High.Location = new System.Drawing.Point(992, 492);
             this.High.Name = "High";
             this.High.Size = new System.Drawing.Size(112, 34);
             this.High.TabIndex = 11;
@@ -146,40 +139,52 @@
             // 
             // low
             // 
-            this.low.Location = new System.Drawing.Point(859, 567);
+            this.low.Location = new System.Drawing.Point(689, 554);
             this.low.Name = "low";
             this.low.Size = new System.Drawing.Size(112, 34);
             this.low.TabIndex = 12;
             this.low.Text = "Low";
             this.low.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // BetAmount
             // 
-            this.textBox2.Location = new System.Drawing.Point(147, 501);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(150, 31);
-            this.textBox2.TabIndex = 13;
+            this.BetAmount.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BetAmount.Location = new System.Drawing.Point(233, 554);
+            this.BetAmount.Name = "BetAmount";
+            this.BetAmount.Size = new System.Drawing.Size(132, 47);
+            this.BetAmount.TabIndex = 13;
+            this.BetAmount.Text = "Place Bet";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(36, 554);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 41);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Bet Amount: ";
             // 
             // Roulette
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1168, 642);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BetAmount);
             this.Controls.Add(this.low);
             this.Controls.Add(this.High);
             this.Controls.Add(this.CB);
             this.Controls.Add(this.custom);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.num);
             this.Controls.Add(this.thirty);
             this.Controls.Add(this.twenty);
             this.Controls.Add(this.twelve);
             this.Controls.Add(this.Red);
             this.Controls.Add(this.black);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
             this.Name = "Roulette";
-            this.Text = "Roulette";
+            this.Text = "number";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -187,19 +192,18 @@
         }
 
         #endregion
-
-        private Label label1;
         private PictureBox pictureBox1;
         private Button black;
         private Button Red;
         private Button twelve;
         private Button twenty;
         private Button thirty;
-        private TextBox textBox1;
+        private TextBox num;
         private Button custom;
         private Label CB;
         private Button High;
         private Button low;
-        private TextBox textBox2;
+        private TextBox BetAmount;
+        private Label label1;
     }
 }
--- CasinoSim/CasinoSim/Roulette.cs
+++ CasinoSim/CasinoSim/Roulette.cs
@@ -12,9 +12,16 @@ namespace CasinoSim
 {
     public partial class Roulette : Form
     {
+        Random r = new Random();
+
         public Roulette()
         {
             InitializeComponent();
+        }
+
+        private void custom_Click(object sender, EventArgs e)
+        {
+            CB.Text = "Bet Square: " + num.Text;
         }
     }
 }
