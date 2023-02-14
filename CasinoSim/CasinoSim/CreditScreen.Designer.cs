4433e5a1a912c34ffe563b97f73f5db9dc609a51
J-Brick <100317351+J-Brick@users.noreply.github.com>
2023-02-14 10:25:57 -0700

    kjd
    ajd

---
+++ CasinoSim/CasinoSim/CreditScreen.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace CasinoSim
+{
+    partial class CreditScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreditNameGav = new System.Windows.Forms.Label();
+            this.CreditationJon = new System.Windows.Forms.Label();
+            this.CreditationDar = new System.Windows.Forms.Label();
+            this.CreditNameDar = new System.Windows.Forms.Label();
+            this.CreditNameJon = new System.Windows.Forms.Label();
+            this.CreditNameIss = new System.Windows.Forms.Label();
+            this.CreditationIss = new System.Windows.Forms.Label();
+            this.CreditationGav = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CreditNameGav
+            // 
+            this.CreditNameGav.AutoSize = true;
+            this.CreditNameGav.Location = new System.Drawing.Point(56, 72);
+            this.CreditNameGav.Name = "CreditNameGav";
+            this.CreditNameGav.Size = new System.Drawing.Size(113, 25);
+            this.CreditNameGav.TabIndex = 0;
+            this.CreditNameGav.Text = "Gavin Zellner";
+            // 
+            // CreditationJon
+            // 
+            this.CreditationJon.AutoSize = true;
+            this.CreditationJon.Location = new System.Drawing.Point(291, 269);
+            this.CreditationJon.Name = "CreditationJon";
+            this.CreditationJon.Size = new System.Drawing.Size(116, 25);
+            this.CreditationJon.TabIndex = 1;
+            this.CreditationJon.Text = "Credit Screen";
+            // 
+            // CreditationDar
+            // 
+            this.CreditationDar.AutoSize = true;
+            this.CreditationDar.Location = new System.Drawing.Point(291, 380);
+            this.CreditationDar.Name = "CreditationDar";
+            this.CreditationDar.Size = new System.Drawing.Size(129, 25);
+            this.CreditationDar.TabIndex = 2;
+            this.CreditationDar.Text = "Money System";
+            // 
+            // CreditNameDar
+            // 
+            this.CreditNameDar.AutoSize = true;
+            this.CreditNameDar.Location = new System.Drawing.Point(56, 380);
+            this.CreditNameDar.Name = "CreditNameDar";
+            this.CreditNameDar.Size = new System.Drawing.Size(130, 25);
+            this.CreditNameDar.TabIndex = 3;
+            this.CreditNameDar.Text = "Darion Quarles";
+            // 
+            // CreditNameJon
+            // 
+            this.CreditNameJon.AutoSize = true;
+            this.CreditNameJon.Location = new System.Drawing.Point(56, 269);
+            this.CreditNameJon.Name = "CreditNameJon";
+            this.CreditNameJon.Size = new System.Drawing.Size(178, 25);
+            this.CreditNameJon.TabIndex = 4;
+            this.CreditNameJon.Text = "Jonathan Brickwedde";
+            // 
+            // CreditNameIss
+            // 
+            this.CreditNameIss.AutoSize = true;
+            this.CreditNameIss.Location = new System.Drawing.Point(56, 162);
+            this.CreditNameIss.Name = "CreditNameIss";
+            this.CreditNameIss.Size = new System.Drawing.Size(118, 25);
+            this.CreditNameIss.TabIndex = 5;
+            this.CreditNameIss.Text = "Issac Morales";
+            // 
+            // CreditationIss
+            // 
+            this.CreditationIss.AutoSize = true;
+            this.CreditationIss.Location = new System.Drawing.Point(291, 162);
+            this.CreditationIss.Name = "CreditationIss";
+            this.CreditationIss.Size = new System.Drawing.Size(101, 25);
+            this.CreditationIss.TabIndex = 7;
+            this.CreditationIss.Text = "Title Screen";
+            // 
+            // CreditationGav
+            // 
+            this.CreditationGav.AutoSize = true;
+            this.CreditationGav.Location = new System.Drawing.Point(291, 405);
+            this.CreditationGav.Name = "CreditationGav";
+            this.CreditationGav.Size = new System.Drawing.Size(51, 25);
+            this.CreditationGav.TabIndex = 8;
+            this.CreditationGav.Text = "Slots";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(291, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Slots";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Roulette";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Roulette";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(291, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Title Screen";
+            // 
+            // CreditScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 847);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CreditationGav);
+            this.Controls.Add(this.CreditationIss);
+            this.Controls.Add(this.CreditNameIss);
+            this.Controls.Add(this.CreditNameJon);
+            this.Controls.Add(this.CreditNameDar);
+            this.Controls.Add(this.CreditationDar);
+            this.Controls.Add(this.CreditationJon);
+            this.Controls.Add(this.CreditNameGav);
+            this.Name = "CreditScreen";
+            this.Text = "CreditScreen";
+            this.Load += new System.EventHandler(this.CreditScreen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label CreditNameGav;
+        private Label CreditationJon;
+        private Label CreditationDar;
+        private Label CreditNameDar;
+        private Label CreditNameJon;
+        private Label CreditNameIss;
+        private Label CreditationIss;
+        private Label CreditationGav;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
