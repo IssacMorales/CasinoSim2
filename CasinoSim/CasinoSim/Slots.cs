f61390f121478f0bc19bf0e39f27a73350134c95
Darion-Quarles <dquarles@student.neumont.edu>
2023-02-21 10:07:01 -0700

    fixed

--- CasinoSim/CasinoSim/Slots.cs
+++ CasinoSim/CasinoSim/Slots.cs
@@ -12,6 +12,9 @@ namespace CasinoSim
 {
     public partial class Slots : Form
     {
+        int img1;
+        int img2;
+        int img3;
         public Slots()
         {
             InitializeComponent();
@@ -35,11 +38,142 @@ namespace CasinoSim
                     {
                         pictureBox1.Dispose();
                         pictureBox1.Image = null;
+                        img1= 1;
                     }
                     else
                     {
                         pictureBox1.Image=Image.FromFile("resource/slot options/r.jpg");
-                        pictureBox1.SizeMode=PictureBoxSizeMode.StretchImage;   
+                        pictureBox1.SizeMode=PictureBoxSizeMode.StretchImage;
+                        img1 = 1;
+
+                    }
+                    break;
+                case 2:
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Dispose();
+                        pictureBox1.Image = null;
+                        img1 = 1;
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Image.FromFile("resource/slot options/r.jpg");
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        img1 = 1;
+
+                    }
+                    break;
+                case 3:
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Dispose();
+                        pictureBox1.Image = null;
+                        img1 = 1;
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Image.FromFile("resource/slot options/r.jpg");
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        img1 = 1;
+
+                    }
+                    break;
+                case 4:
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Dispose();
+                        pictureBox1.Image = null;
+                        img1 = 1;
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Image.FromFile("resource/slot options/r.jpg");
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        img1 = 1;
+
+                    }
+                    break;
+                case 5:
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Dispose();
+                        pictureBox1.Image = null;
+                        img1 = 1;
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Image.FromFile("resource/slot options/r.jpg");
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        img1 = 1;
+
+                    }
+                    break;
+                case 6:
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Dispose();
+                        pictureBox1.Image = null;
+                        img1 = 1;
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Image.FromFile("resource/slot options/r.jpg");
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        img1 = 1;
+
+                    }
+                    break;
+                case 7:
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Dispose();
+                        pictureBox1.Image = null;
+                        img1 = 1;
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Image.FromFile("resource/slot options/r.jpg");
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        img1 = 1;
+
+                    }
+                    break;
+                case 8:
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Dispose();
+                        pictureBox1.Image = null;
+                        img1 = 1;
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Image.FromFile("resource/slot options/r.jpg");
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        img1 = 1;
+
+                    }
+                    break;
+                case 9:
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Dispose();
+                        pictureBox1.Image = null;
+                        img1 = 1;
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Image.FromFile("resource/slot options/chad.jpg");
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        img1 = 1;
+
                     }
                     break;
             }
