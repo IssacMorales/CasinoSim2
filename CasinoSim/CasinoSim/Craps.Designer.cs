0f7cfe683a2f414f8f1ada783ec0290ba3a90c49
IssacMorales <imorales@student.neumont.edu>
2023-02-21 17:05:28 -0700

    asdf

--- CasinoSim/CasinoSim/Craps.Designer.cs
+++ CasinoSim/CasinoSim/Craps.Designer.cs
@@ -31,13 +31,16 @@
             this.PL = new System.Windows.Forms.Button();
             this.DPL = new System.Windows.Forms.Button();
             this.lblRoll = new System.Windows.Forms.Label();
+            this.CB = new System.Windows.Forms.Button();
+            this.DCB = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // PL
             // 
-            this.PL.Location = new System.Drawing.Point(402, 278);
+            this.PL.Location = new System.Drawing.Point(574, 463);
+            this.PL.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PL.Name = "PL";
-            this.PL.Size = new System.Drawing.Size(104, 23);
+            this.PL.Size = new System.Drawing.Size(149, 38);
             this.PL.TabIndex = 0;
             this.PL.Text = "Pass Line";
             this.PL.UseVisualStyleBackColor = true;
@@ -45,9 +48,10 @@
             // 
             // DPL
             // 
-            this.DPL.Location = new System.Drawing.Point(402, 324);
+            this.DPL.Location = new System.Drawing.Point(574, 540);
+            this.DPL.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.DPL.Name = "DPL";
-            this.DPL.Size = new System.Drawing.Size(104, 23);
+            this.DPL.Size = new System.Drawing.Size(149, 38);
             this.DPL.TabIndex = 1;
             this.DPL.Text = "Don\'t Pass Line";
             this.DPL.UseVisualStyleBackColor = true;
@@ -56,20 +60,48 @@
             // lblRoll
             // 
             this.lblRoll.AutoSize = true;
-            this.lblRoll.Location = new System.Drawing.Point(323, 95);
+            this.lblRoll.Location = new System.Drawing.Point(461, 158);
+            this.lblRoll.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRoll.Name = "lblRoll";
-            this.lblRoll.Size = new System.Drawing.Size(51, 15);
+            this.lblRoll.Size = new System.Drawing.Size(77, 25);
             this.lblRoll.TabIndex = 2;
             this.lblRoll.Text = "Number";
             // 
+            // CB
+            // 
+            this.CB.Enabled = false;
+            this.CB.Location = new System.Drawing.Point(757, 463);
+            this.CB.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.CB.Name = "CB";
+            this.CB.Size = new System.Drawing.Size(149, 38);
+            this.CB.TabIndex = 3;
+            this.CB.Text = "Come Bet";
+            this.CB.UseVisualStyleBackColor = true;
+            this.CB.Click += new System.EventHandler(this.CB_Click);
+            // 
+            // DCB
+            // 
+            this.DCB.Enabled = false;
+            this.DCB.Location = new System.Drawing.Point(757, 540);
+            this.DCB.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.DCB.Name = "DCB";
+            this.DCB.Size = new System.Drawing.Size(149, 38);
+            this.DCB.TabIndex = 4;
+            this.DCB.Text = "Don\'t Come Bet";
+            this.DCB.UseVisualStyleBackColor = true;
+            this.DCB.Click += new System.EventHandler(this.DCB_Click);
+            // 
             // Craps
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1143, 750);
+            this.Controls.Add(this.DCB);
+            this.Controls.Add(this.CB);
             this.Controls.Add(this.lblRoll);
             this.Controls.Add(this.DPL);
             this.Controls.Add(this.PL);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Craps";
             this.Text = "Craps";
             this.ResumeLayout(false);
@@ -82,5 +114,7 @@
         private Button PL;
         private Button DPL;
         private Label lblRoll;
+        private Button CB;
+        private Button DCB;
     }
 }
--- CasinoSim/CasinoSim/Craps.cs
+++ CasinoSim/CasinoSim/Craps.cs
@@ -33,11 +33,16 @@ namespace CasinoSim
                 lblRoll.Text = "Winner";
                 PL.Enabled = false;
                 DPL.Enabled = false;
-            }else if (roll == 2 || roll == 3 || roll == 12)
+                CB.Enabled = true;
+                DCB.Enabled = true;
+            }
+            else if (roll == 2 || roll == 3 || roll == 12)
             {
                 lblRoll.Text = "Loss";
                 PL.Enabled = false;
                 DPL.Enabled = false;
+                CB.Enabled = true;
+                DCB.Enabled = true;
             }
             else
             {
@@ -62,17 +67,74 @@ namespace CasinoSim
                 lblRoll.Text = "Loss";
                 PL.Enabled = false;
                 DPL.Enabled = false;
+                CB.Enabled = true;
+                DCB.Enabled = true;
             }
             else if (roll == 2 || roll == 3 || roll == 12)
             {
                 lblRoll.Text = "Winner";
                 PL.Enabled = false;
                 DPL.Enabled = false;
+                CB.Enabled = true;
+                DCB.Enabled = true;
             }
             else
             {
                 lblRoll.Text = roll.ToString();
                 point++;
+            }
+        }
+
+        private void CB_Click(object sender, EventArgs e)
+        {
+            if(point <= 0)
+            {
+                lblRoll.Text = "No points";
+            }
+            else
+            {
+                DoRoll();
+                if (roll == 7 || roll == 11)
+                {
+                    lblRoll.Text = "Winner";
+                }
+                else if (roll == 2 || roll == 3 || roll == 12)
+                {
+                    lblRoll.Text = "Loss";
+                    point--;
+                }
+                else
+                {
+                    lblRoll.Text = roll.ToString();
+                    point--;
+                }
+            }
+        }
+
+        private void DCB_Click(object sender, EventArgs e)
+        {
+            if (point <= 0)
+            {
+                lblRoll.Text = "No points";
+            }
+            else
+            {
+                DoRoll();
+                if (roll == 7 || roll == 11)
+                {
+                    lblRoll.Text = "Loss";
+                    point--;
+                }
+                else if (roll == 2 || roll == 3 || roll == 12)
+                {
+                    lblRoll.Text = "Winner";
+                    point--;
+                }
+                else
+                {
+                    lblRoll.Text = roll.ToString();
+                    point--;
+                }
             }
         }
     }
