4539f6501a95146c3709516a92315e4013f8c641
J-Brick <100317351+J-Brick@users.noreply.github.com>
2023-02-27 09:43:04 -0700

    f
    f

--- CasinoSim/CasinoSim/CreditScreen.Designer.cs
+++ CasinoSim/CasinoSim/CreditScreen.Designer.cs
@@ -42,6 +42,9 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // CreditNameGav
@@ -157,24 +160,54 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(291, 212);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 25);
+            this.label5.Size = new System.Drawing.Size(57, 25);
             this.label5.TabIndex = 13;
-            this.label5.Text = "Slots";
+            this.label5.Text = "Craps";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(291, 319);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 25);
+            this.label6.Size = new System.Drawing.Size(57, 25);
             this.label6.TabIndex = 14;
-            this.label6.Text = "Slots";
+            this.label6.Text = "Craps";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(398, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 25);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Black Jack";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(297, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 25);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Slots";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(291, 430);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 25);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Black Jack";
             // 
             // CreditScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1419, 847);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -213,5 +246,8 @@
         private Label label4;
         private Label label5;
         private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
     }
 }
--- CasinoSim/CasinoSim/CreditScreen.cs
+++ CasinoSim/CasinoSim/CreditScreen.cs
@@ -21,5 +21,7 @@ namespace CasinoSim
         {
 
         }
+
+       
     }
 }
