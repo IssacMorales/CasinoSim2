08c2c8a1ee2f0d9455c85880f955271e930c1759
xznosage2 <gzellner@student.neumont.edu>
2023-03-08 03:01:05 -0700

    changes

--- CasinoSim/CasinoSim/bJack.Designer.cs
+++ CasinoSim/CasinoSim/bJack.Designer.cs
@@ -28,18 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            hitBtn = new Button();
+            standBtn = new Button();
+            betBtn = new Button();
+            DDBtn = new Button();
+            splitBtn = new Button();
+            cMoney = new Label();
+            moneyBox = new TextBox();
+            blackjacLlabel = new Label();
             SuspendLayout();
+            // 
+            // hitBtn
+            // 
+            hitBtn.Location = new Point(985, 487);
+            hitBtn.Name = "hitBtn";
+            hitBtn.Size = new Size(112, 34);
+            hitBtn.TabIndex = 0;
+            hitBtn.Text = "Hit";
+            hitBtn.UseVisualStyleBackColor = true;
+            hitBtn.Click += hitBtn_Click;
+            // 
+            // standBtn
+            // 
+            standBtn.Location = new Point(985, 560);
+            standBtn.Name = "standBtn";
+            standBtn.Size = new Size(112, 34);
+            standBtn.TabIndex = 1;
+            standBtn.Text = "Stand";
+            standBtn.UseVisualStyleBackColor = true;
+            standBtn.Click += standBtn_Click;
+            // 
+            // betBtn
+            // 
+            betBtn.Location = new Point(39, 574);
+            betBtn.Name = "betBtn";
+            betBtn.Size = new Size(112, 34);
+            betBtn.TabIndex = 2;
+            betBtn.Text = "Bet";
+            betBtn.UseVisualStyleBackColor = true;
+            betBtn.Click += betBtn_Click;
+            // 
+            // DDBtn
+            // 
+            DDBtn.Location = new Point(207, 574);
+            DDBtn.Name = "DDBtn";
+            DDBtn.Size = new Size(148, 34);
+            DDBtn.TabIndex = 3;
+            DDBtn.Text = "Dubble Down";
+            DDBtn.UseVisualStyleBackColor = true;
+            DDBtn.Click += DDBtn_Click;
+            // 
+            // splitBtn
+            // 
+            splitBtn.Location = new Point(422, 574);
+            splitBtn.Name = "splitBtn";
+            splitBtn.Size = new Size(112, 34);
+            splitBtn.TabIndex = 4;
+            splitBtn.Text = "Split Hand";
+            splitBtn.UseVisualStyleBackColor = true;
+            splitBtn.Click += splitBtn_Click;
+            // 
+            // cMoney
+            // 
+            cMoney.AutoSize = true;
+            cMoney.Location = new Point(568, 579);
+            cMoney.Name = "cMoney";
+            cMoney.Size = new Size(71, 25);
+            cMoney.TabIndex = 5;
+            cMoney.Text = "Money:";
+            // 
+            // moneyBox
+            // 
+            moneyBox.Location = new Point(652, 579);
+            moneyBox.Name = "moneyBox";
+            moneyBox.Size = new Size(150, 31);
+            moneyBox.TabIndex = 6;
+            // 
+            // blackjacLlabel
+            // 
+            blackjacLlabel.AutoSize = true;
+            blackjacLlabel.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            blackjacLlabel.Location = new Point(425, 482);
+            blackjacLlabel.Name = "blackjacLlabel";
+            blackjacLlabel.Size = new Size(0, 54);
+            blackjacLlabel.TabIndex = 7;
             // 
             // bJack
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1238, 652);
+            Controls.Add(blackjacLlabel);
+            Controls.Add(moneyBox);
+            Controls.Add(cMoney);
+            Controls.Add(splitBtn);
+            Controls.Add(DDBtn);
+            Controls.Add(betBtn);
+            Controls.Add(standBtn);
+            Controls.Add(hitBtn);
             Name = "bJack";
             Text = "Form2";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button hitBtn;
+        private Button standBtn;
+        private Button betBtn;
+        private Button DDBtn;
+        private Button splitBtn;
+        private Label cMoney;
+        private TextBox moneyBox;
+        private Label blackjacLlabel;
     }
 }
--- CasinoSim/CasinoSim/bJack.cs
+++ CasinoSim/CasinoSim/bJack.cs
@@ -1,20 +1,340 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CasinoSim
+﻿namespace CasinoSim
 {
     public partial class bJack : Form
     {
+
+        private static int staticMoney()
+        {
+            return Money.buck;
+        }
+
+
+        int money = staticMoney();
+        private void moneyEqual()
+        {
+            //Money.buck = money;
+            cMoney.Text = "Money: " + Money.buck.ToString();
+
+        }
+        private void addMoney(int money)
+        {
+            Money.buck += money;
+            cMoney.Text = "Money: " + Money.buck.ToString();
+        }
+        private void loseMoney(int money)
+        {
+            Money.buck -= money;
+            cMoney.Text = "Money: " + Money.buck.ToString();
+
+
+        }
+
+
+
+        List<Cards> deck = new List<Cards>();
+        List<Cards> hand = new List<Cards>();
+        List<Cards> splitHand = new List<Cards>();
+        List<Cards> dealerhand = new List<Cards>();
+        bool boolSplitHand = false;
+        bool boolStand = false;
+        int dealerhandValue;
+        int handValue;
+        int splitHandValue;
+        int betAmount;
+
+        public enum TurnCount
+        {
+            PlayerTurn,
+            EnemyTurn
+        }
+        TurnCount turn = TurnCount.PlayerTurn;
+
+
         public bJack()
         {
             InitializeComponent();
+            moneyEqual();
+
+            for (int i = 1; i < 14; i++)
+            {
+                Cards card = new Cards(i, "h");
+                deck.Add(card);
+            }
+            for (int i = 1; i < 14; i++)
+            {
+                Cards card = new Cards(i, "d");
+                deck.Add(card);
+            }
+            for (int i = 1; i < 14; i++)
+            {
+                Cards card = new Cards(i, "c");
+                deck.Add(card);
+            }
+            for (int i = 1; i < 14; i++)
+            {
+                Cards card = new Cards(i, "s");
+                deck.Add(card);
+            }
+
+            addToDelearHand(2);
+            addToHand(2);
+
+        }
+        private void nextTurn()
+        {
+            if (Money.buck>=0)
+            {
+                Form1 form = new Form1();
+                form.Show();
+                this.Hide();
+            }
+            blackjacLlabel.Text = "";
+
+            moneyEqual();
+            if (turn.Equals(TurnCount.EnemyTurn))
+            {
+
+                foreach (Cards item in dealerhand)
+                {
+                    if (item.ToString().Equals("1"))
+                    {
+                        item.Ace = true;
+                    }
+                    int aceTotal = dealerhandValue + 10;
+                    if (item.Ace == true && aceTotal > 22)
+                    {
+                        dealerhandValue += 10;
+                    }
+
+                }
+
+
+                if (dealerhandValue < 17)
+                {
+                    addToDelearHand(1);
+                    turn = TurnCount.PlayerTurn;
+                }
+                else if (dealerhandValue > 21)
+                {
+                    winner();
+
+                }
+                else if (dealerhandValue == 21)
+                {
+                    loser();
+                }
+
+            }
+
+            else if (turn.Equals(TurnCount.PlayerTurn))
+            {
+                foreach (Cards item in hand)
+                {
+                    if (item.ToString().Equals("1"))
+                    {
+                        item.Ace = true;
+                    }
+                    int aceTotal = handValue + 10;
+
+                    if (item.Ace == true && aceTotal > 22)
+                    {
+                        handValue += 10;
+                    }
+                }
+
+                if (boolStand && dealerhandValue > handValue)
+                {
+                    loser();
+                }
+
+                if (handValue > 21)
+                {
+                    loser();
+                }
+                if (handValue < 21 && hand.Count >= 5)
+                {
+                    winner();
+                }
+                if (handValue == 21)
+                {
+                    winner();
+                }
+
+                if (boolSplitHand)
+                {
+                    {
+                        foreach (Cards item in splitHand)
+                        {
+                            if (item.ToString().Equals("1"))
+                            {
+                                item.Ace = true;
+                            }
+                            int aceTotal = splitHandValue + 10;
+
+                            if (item.Ace == true && aceTotal > 22)
+                            {
+                                splitHandValue += 10;
+                            }
+                        }
+
+                        if (boolStand && dealerhandValue > splitHandValue)
+                        {
+                            loser();
+                        }
+
+                        if (splitHandValue > 21)
+                        {
+                            loser();
+                        }
+                        if (splitHandValue < 21 && splitHand.Count >= 5)
+                        {
+                            winner();
+                        }
+                        if (splitHandValue == 21)
+                        {
+                            winner();
+                        }
+                    }
+                }
+
+                Console.WriteLine("Your hand value: " + handValue + " dealer hand value: " + dealerhandValue);
+            }
+        }
+
+
+        public void winner()
+        {
+            addMoney(betAmount * 2);
+            betAmount = 0;
+
+
+        }
+        public void winner(string blackjack)
+        {
+            addMoney(betAmount * 3);
+            blackjacLlabel.Text = "BlackJack!!!";
+            betAmount = 0;
+
+        }
+        public void loser()
+        {
+            betAmount = 0;
+        }
+
+
+        private void bJack_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DDBtn_Click(object sender, EventArgs e)
+        {
+            betAmount *= 2;
+            boolStand = true;
+            addToHand(1);
+        }
+
+        private void splitBtn_Click(object sender, EventArgs e)
+        {
+            if (hand.Count > 3)
+            {
+                int card1 = int.Parse(hand[0].ToString());
+
+                int card2 = int.Parse(hand[1].ToString());
+
+                if (card1 == card2)
+                {
+                    boolSplitHand = true;
+                    addToHand(1);
+                }
+            }
+        }
+
+        private void betBtn_Click(object sender, EventArgs e)
+        {
+            string moneyText = moneyBox.Text;
+            int betAmount = int.Parse(moneyText);
+
+            loseMoney(betAmount);
+
+            moneyEqual();
+        }
+
+        private void hitBtn_Click(object sender, EventArgs e)
+        {
+            if (turn.Equals(TurnCount.PlayerTurn))
+            {
+
+                addToHand(1);
+            }
+            turn = TurnCount.EnemyTurn;
+
+        }
+
+        private void standBtn_Click(object sender, EventArgs e)
+        {
+            if (turn.Equals(TurnCount.PlayerTurn))
+            {
+                addToHand(0);
+                turn = TurnCount.EnemyTurn;
+            }
+        }
+
+        private void addToHand(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                Random r = new Random();
+                int ran = r.Next(0, deck.Count - 1);
+                hand.Add(deck[ran]);
+                deck.RemoveAt(ran);
+
+                if (boolSplitHand)
+                {
+                    ran = r.Next(0, deck.Count - 1);
+                    splitHand.Add(deck[ran]);
+                    deck.RemoveAt(ran);
+                }
+
+
+            }
+            handValue = setValueOfHand(hand);
+            if (boolSplitHand)
+            {
+                splitHandValue = setValueOfHand(splitHand);
+            }
+        }
+        private void addToDelearHand(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                Random r = new Random();
+                int ran = r.Next(0, deck.Count - 1);
+                dealerhand.Add(deck[ran]);
+                deck.RemoveAt(ran);
+                dealerhandValue = setValueOfHand(dealerhand);
+            }
+        }
+
+        private void betAmount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public int setValueOfHand(List<Cards> hand)
+        {
+            int totalValue = 0;
+            foreach (var item in hand)
+            {
+                totalValue += item.returnValue();
+            }
+            return totalValue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
+
 }
